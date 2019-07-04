52370bece435552cc3a2b7dd2d81f81ae49ddfff
Vishal Kumar Singh <01vksingh@gmail.com>
2019-07-04 11:21:18 +0530

    food app

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.food;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox pi;
+    private CheckBox co;
+    private CheckBox ice;
+    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pi = findViewById(R.id.checkBox);
+        co = findViewById(R.id.checkBox2);
+        ice = findViewById(R.id.checkBox3);
+
     }
 }
